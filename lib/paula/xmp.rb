4362d5c279268e64a89592cbf970811bc4e55e2a
Misty De Meo <mistydemeo@gmail.com>
2012-11-12 14:36:37 -0600

    Implement XMP plugin using current Player API

--- lib/paula/xmp.rb
+++ lib/paula/xmp.rb
@@ -1,8 +1,26 @@
+require 'paula/library'
+require 'paula/player'
+
 require 'ffi'
 
 module XMP
   extend FFI::Library
   ffi_lib 'xmp'
+
+  class XmpChannel < FFI::Struct
+    layout(
+           :pan, :int,
+           :vol, :int,
+           :flg, :int
+    )
+  end
+
+  class XmpPattern < FFI::Struct
+    layout(
+           :rows, :int,
+           :index, [:int, 1]
+    )
+  end
 
   class XmpEvent < FFI::Struct
     layout(
@@ -14,6 +32,105 @@ module XMP
            :f2t, :uchar,
            :f2p, :uchar,
            :_flag, :uchar
+    )
+  end
+
+  class XmpTrack < FFI::Struct
+    layout(
+           :rows, :int,
+           :event, [XmpEvent, 1]
+    )
+  end
+
+  class XmpEnvelope < FFI::Struct
+    layout(
+           :flg, :int,
+           :npt, :int,
+           :scl, :int,
+           :sus, :int,
+           :sue, :int,
+           :lps, :int,
+           :lpe, :int,
+           :data, [:short, 32*2]
+    )
+  end
+
+  class XmpInstrumentMap < FFI::Struct
+    layout(
+           :ins, :uchar,
+           :xpo, :char
+    )
+  end
+
+  class XmpInstrumentSub < FFI::Struct
+    layout(
+           :vol, :int,
+           :gvl, :int,
+           :pan, :int,
+           :xpo, :int,
+           :fin, :int,
+           :vwf, :int,
+           :vde, :int,
+           :vra, :int,
+           :vsw, :int,
+           :rvv, :int,
+           :sid, :int,
+           :nna, :int,
+           :dct, :int,
+           :dca, :int,
+           :ifc, :int,
+           :ifr, :int,
+           :hld, :int
+    )
+  end
+
+  class XmpInstrument < FFI::Struct
+    layout(
+           :name, [:char, 32],
+           :vol, :int,
+           :nsm, :int,
+           :rls, :int,
+           :aei, XmpEnvelope,
+           :pei, XmpEnvelope,
+           :fei, XmpEnvelope,
+           :vts, :int,
+           :wts, :int,
+           :map, [XmpInstrumentMap, 121],
+           :sub, XmpInstrumentSub
+    )
+  end
+
+  class XmpSample < FFI::Struct
+    layout(
+           :name, [:char, 32],
+           :len, :int,
+           :lps, :int,
+           :lpe, :int,
+           :flg, :int,
+           :data, :pointer
+    )
+  end
+
+  class XmpModule < FFI::Struct
+    layout(
+           :name, [:char, 64],
+           :type, [:char, 64],
+           :pat, :int,
+           :trk, :int,
+           :chn, :int,
+           :ins, :int,
+           :smp, :int,
+           :spd, :int,
+           :bpm, :int,
+           :len, :int,
+           :rst, :int,
+           :gvl, :int,
+           :xxp, XmpPattern,
+           :xxt, XmpTrack,
+           :xxi, XmpInstrument,
+           :xxs, XmpSample,
+           :xxc, [XmpChannel, 64],
+           :xxo, [:uchar, 256]
     )
   end
 
@@ -29,6 +146,13 @@ module XMP
            :pan, :uchar,
            :reserved, :uchar,
            :event, XmpEvent
+    )
+  end
+
+  class XmpSequence < FFI::Struct
+    layout(
+           :entry_point, :int,
+           :duration, :int
     )
   end
 
@@ -52,12 +176,12 @@ module XMP
            :virt_channels, :int,
            :virt_used, :int,
            :vol_base, :int,
-           :mod, :pointer,
+           :channel_info, [XmpModuleInfoChannelInfo, 64],
+           :mod, XmpModule,
            :comment, :pointer,
            :sequence, :int,
            :num_sequences, :int,
-           :seq_data, :pointer,
-           :channel_info, [XmpModuleInfoChannelInfo, 64]
+           :seq_data, XmpSequence
     )
 
     def buffer
@@ -77,4 +201,52 @@ module XMP
   attach_function :xmp_inject_event, [ :pointer, :int, :pointer ], :void
   attach_function :xmp_get_format_list, [  ], :string
   attach_function :xmp_control, [ :pointer, :int, :varargs ], :int
+
+  class Player < Paula::Player
+    # TODO: add the full list of XMP's extensions
+    extensions %w[mod xm]
+    maximum_frequency 48000
+    supports_title
+    supports_comment
+    supports_instruments
+
+    def self.finalize ptr
+      proc do
+        XMP.xmp_player_end ptr
+        XMP.xmp_release_module ptr
+        XMP.xmp_free_context ptr
+      end
+    end
+
+    def initialize file, opts
+      super
+
+      @context = XMP.xmp_create_context
+      @info    = XMP::XmpModuleInfo.new
+
+      if !XMP.xmp_load_module @context, file
+        raise Paula::LoadError, "could not open file #{file}"
+      end
+
+      XMP.xmp_player_start @context, opts[:frequency], 0
+      XMP.xmp_player_get_info @context, @info
+
+      ObjectSpace.define_finalizer(self, self.class.finalize(@context))
+    end
+
+    def next_sample
+      XMP.xmp_player_frame @context
+      XMP.xmp_player_get_info @context, @info
+      @info.buffer
+    end
+
+    # TODO: implement timeout
+    def complete?
+      @info[:loop_count] > @loops
+    end
+
+    def duration
+      @info[:total_time]
+    end
+  end
 end
