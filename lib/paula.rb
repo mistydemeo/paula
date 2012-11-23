cc2100a7e8062527d276b92a0e4ec8a0afe3e1e7
Misty De Meo <mistydemeo@gmail.com>
2012-11-22 19:36:04 -0600

    Load plugins in paula.rb, if available

--- lib/paula.rb
+++ lib/paula.rb
@@ -1,3 +1,14 @@
 require 'paula/core'
 require 'paula/player'
 require "paula/version"
+
+# FFI libraries raise a LoadError if the required dynamic library
+# can't be loaded. We don't require that the user have the lib
+# for every supported library, of course.
+['paula/mdxmini', 'paula/xmp'].each do |file|
+  begin
+    require file
+  rescue LoadError
+    next
+  end
+end
