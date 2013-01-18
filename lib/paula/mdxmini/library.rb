ce9dbe4808b35afd40a2fe2322ffe7522e3b16b0
Misty De Meo <mistydemeo@gmail.com>
2013-01-17 19:39:12 -0600

    mdxmini: reflect API changes from 20130115

--- lib/paula/mdxmini/library.rb
+++ lib/paula/mdxmini/library.rb
@@ -11,7 +11,7 @@ module Paula
     attach_function :mdx_disp_info, [:pointer], :void
     attach_function :mdx_next_frame, [:pointer], :int
     attach_function :mdx_frame_length, [:pointer], :int
-    attach_function :mdx_make_buffer, [:short, :int], :void
+    attach_function :mdx_make_buffer, [:pointer, :short, :int], :void
     attach_function :mdx_calc_sample, [:pointer, :pointer, :int], :int
     attach_function :mdx_get_title, [:pointer, :pointer], :void
     attach_function :mdx_get_length, [:pointer], :int
@@ -21,13 +21,23 @@ module Paula
     attach_function :mdx_get_sample_size, [:pointer], :int
     attach_function :mdx_get_buffer_size, [:pointer], :int
 
+    class Songdata < FFI::Struct
+      layout(
+        :mdx2151, :pointer,
+        :mdxmml_ym2151, :pointer,
+        :pcm8, :pointer,
+        :ym2151_c, :pointer
+      )
+    end
+
     class T_mdxmini < FFI::Struct
       layout(
         :samples, :int,
         :channels, :int,
         :mdx, :pointer,
         :pdx, :pointer,
-        :self, :pointer
+        :self, :pointer,
+        :songdata, Songdata.ptr
       )
     end
   end
